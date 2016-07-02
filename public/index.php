be15ca98e5ca985f240dc54fb2e869d98bb21c03
trangnguyenhust95 <trangnguyen.hust.95@gmail.com>
2016-07-01 17:04:39 -0700

    sua login

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-//session_start ();
+session_start ();
 
 require_once '../src/header.php';
 
 if (isset ( $_GET ['action'] ) and $_GET ['action'] == 'logout') {
-	setcookie("username",$_POST['username'],time()-1800);
+	
 	setcookie("flag",true,time()-1800);
 	header ( 'location: /project-tt/demo5/public/index.php?action=login' );
 	return;
@@ -20,9 +20,10 @@ if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
 		$username = "trangnguyen";
 		$password = "12345";
 		if($_POST['username'] == $username and $_POST['userpass'] == $password){
+			$_SESSION['username'] = 1;
+			setcookie("id", session_id(), time() +1800);
 			setcookie("username",$_POST['username'],time()+1800);
 			setcookie("flag",true,time()+1800);
-		//$_COOKIE['flag'] = true;
 		
 		header ( 'location: /project-tt/demo5/public/index.php' );
 		return;
@@ -31,7 +32,7 @@ if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
 	require_once '../src/login.php';
 } else {
 	
-	if (! isset ( $_COOKIE['flag'] ) or $_COOKIE['flag'] != true) {
+	if (! isset ( $_COOKIE['flag'] ) or $_COOKIE['flag'] != true or $_SESSION['username'] != 1) {
 		
 		header ( 'location: /project-tt/demo5/public/index.php?action=login' );
 		return;
