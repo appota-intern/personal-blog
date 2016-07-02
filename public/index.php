c3b92b2f30019df7a54964ce2d3dd13af33eedb1
trangnguyenhust95 <trangnguyen.hust.95@gmail.com>
2016-07-02 00:46:45 -0700

    sua login

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
-session_start ();
+//session_start ();
 
+
+if(!empty($_COOKIE['id'])){
+	session_id($_COOKIE['id']);
+	
+}
+session_start();
 require_once '../src/header.php';
+
+
 
 if (isset ( $_GET ['action'] ) and $_GET ['action'] == 'logout') {
 	
@@ -11,7 +19,7 @@ if (isset ( $_GET ['action'] ) and $_GET ['action'] == 'logout') {
 }
 if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
 	
-	if (isset ( $_COOKIE['flag'] ) and $_COOKIE['flag'] == true) {
+	if (isset ( $_SESSION['flag'] ) and $_SESSION['flag'] == true) {
 		header ( 'location: /project-tt/demo5/public/index.php' );
 		return;
 	}
@@ -20,23 +28,29 @@ if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
 		$username = "trangnguyen";
 		$password = "12345";
 		if($_POST['username'] == $username and $_POST['userpass'] == $password){
-			$_SESSION['username'] = 1;
+			//setcookie()
 			setcookie("id", session_id(), time() +1800);
-			setcookie("username",$_POST['username'],time()+1800);
-			setcookie("flag",true,time()+1800);
+			//session_start();
+		
+			//setcookie("username",$_POST['username'],time()+1800);
+			//("flag",true,time()+1800);
+			$_SESSION['flag'] = true;
+			$_SESSION['username'] = $_POST['username'];
 		
 		header ( 'location: /project-tt/demo5/public/index.php' );
 		return;
 		}
+		
 	}
 	require_once '../src/login.php';
 } else {
 	
-	if (! isset ( $_COOKIE['flag'] ) or $_COOKIE['flag'] != true or $_SESSION['username'] != 1) {
+	if (! isset ( $_SESSION['flag'] ) or $_SESSION['flag'] != true) {
 		
 		header ( 'location: /project-tt/demo5/public/index.php?action=login' );
 		return;
 	}
+	
 	require_once '../src/hello.php';
 }
 
--- src/hello.php
+++ src/hello.php
@@ -1,4 +1,4 @@
 <?php
-echo 'hello';
+echo 'hello' . $_SESSION['username'];
 ?>
 <a href="/project-tt/demo5/public/index.php?action=logout">Logout</a>
