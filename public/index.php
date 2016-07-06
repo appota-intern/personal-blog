54eb347b41dade54fc63e246501bd1df6f16be7e
thnt <coder53@gmail.com>
2016-07-06 10:22:19 +0700

    add files

--- public/index.php
+++ public/index.php
@@ -1,32 +1,59 @@
-<?php 
-	if(!empty($_COOKIE['id'])){
-	session_id($_COOKIE['id']);
-	}
-	session_start();
-	require_once '../src/view/header.php';
-	require_once '../src/controller/UserController.php';
-	$user = new UserControllers;
-	
-	
-	if (isset ( $_GET ['action'] ) and $_GET ['action'] == 'logout'){
-		$user->logout();
-	}
-	if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+$base_uri = getenv('BASE_URI');
+
+$base_uri_len = strlen($base_uri);
+if ($base_uri_len and substr($uri, 0, $base_uri_len) == $base_uri) {
+	$uri = substr($uri, $base_uri_len);
+}
+
+$user = new Controller\UserController();
+
+switch ($uri) {
+	case '/login':
 		$user->login();
-	}
-	
-	else {
-	
-		if (! isset ( $_SESSION['flag'] ) or $_SESSION['flag'] != true) {
-	
-			header ( 'location: /project-tt/public/index.php?action=login' );
-			return;
-		}
-	
+		break;
+	case '/logout':
+		$user->logout();
+		break;
+	case '/':
 		$user->hello();
-	}
-	
-	require_once '../src/view/footer.php';
-	
-?>
+		break;
+	default:
+		die('Page not found');
+		break;
+}
+	// if(!empty($_COOKIE['id'])){
+	// session_id($_COOKIE['id']);
+	// }
+	// session_start();
+	// require_once '../src/view/header.php';
+	// require_once '../src/controller/UserController.php';
+	// $user = new UserControllers;
+
+
+	// if (isset ( $_GET ['action'] ) and $_GET ['action'] == 'logout'){
+	// 	$user->logout();
+	// }
+	// if (isset ( $_GET ['action'] ) && $_GET ['action'] == 'login') {
+	// 	$user->login();
+	// }
+
+	// else {
+
+	// 	if (! isset ( $_SESSION['flag'] ) or $_SESSION['flag'] != true) {
+
+	// 		header ( 'location: /project-tt/public/index.php?action=login' );
+	// 		return;
+	// 	}
+
+	// 	$user->hello();
+	// }
+
+	// require_once '../src/view/footer.php';
+
+
 
