5953451cca6035dbac250e759afe7b21de5f1f80
thnt <coder53@gmail.com>
2016-07-14 17:10:25 +0700

    update header & footer

--- src/view/footer.php
+++ src/view/footer.php
@@ -2,5 +2,8 @@
 <div class="footer">
     This is footer
 </div>
+
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
