980c4a5bd33811a8f2f4ab8d4d670c03f1bea40e
ngohonghai <ngohaibkk57@gmail.com>
2016-07-18 11:11:27 +0700

    Add file template

--- src/view/footer.php
+++ src/view/footer.php
@@ -1,9 +1,20 @@
 
-<div class="footer">
-    This is footer
-</div>
+<!-- Footer -->
+<footer class="text-center">
+    <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+        <span class="glyphicon glyphicon-chevron-up"></span>
+    </a><br><br>
+    <p>Bootstrap Theme Made By <a href="http://www.w3schools.com" data-toggle="tooltip" title="Visit w3schools">www.w3schools.com</a></p> 
+</footer>
 
-<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $("#myBtn").click(function () {
+            $("#myModal").modal();
+        });
+    });
+</script>
 </body>
 </html>
--- src/view/hello.php
+++ src/view/hello.php
@@ -1,6 +1,22 @@
-<?php
-echo 'hello ' . $_SESSION['username'] . '! ';
-?>
 
-<a href="logout">Logout</a>
-
+<div class="container text-center">    
+    <h3>What We Do</h3><br>
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="imgs/baihongvan.jpg" class="img-responsive" style="width:100%" alt="Image">
+            <p>Current Project</p>
+        </div>
+        <div class="col-sm-4"> 
+            <img src="imgs/baibacvan.jpg" class="img-responsive" style="width:100%" alt="Image">
+            <p>Project 2</p>    
+        </div>
+        <div class="col-sm-4">
+            <div class="well">
+                <p>Some text..</p>
+            </div>
+            <div class="well">
+                <p>Some text..</p>
+            </div>
+        </div>
+    </div>
+</div>
