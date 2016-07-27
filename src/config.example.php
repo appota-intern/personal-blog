9564b586d39c3d8466d976343996308dc8b8f9e5
trangnguyenit <trangnguyen95.hust@gmail.com>
2016-07-27 16:13:36 +0700

    register

---
+++ src/config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+putenv('BASE_URI=/public');
+putenv('HOST=localhost');
+putenv('USER_NAME=root');
+putenv('PASSWORD=12344321');
+putenv('DB_NAME=data');
