ef3caff36a70649aa8fc33781a9336e1a0547e09
haducanh <haducanh94@gmail.com>
2016-07-27 11:24:11 +0700

    hash

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,7 +25,7 @@ class UserController extends BaseController {
 
         if (!empty($_POST ['username']) && !empty($_POST ['userpass'])) {
             $username = $_POST['username'];
-            $passwords= md5($_POST['userpass']);
+            $passwords= password_hash($_POST['userpass'], PASSWORD_DEFAULT);
 
             $usermodel = new model\UserModel();
 
@@ -71,7 +71,7 @@ class UserController extends BaseController {
         
         if(!empty($_POST ['name']) && !empty($_POST ['pass']) && !empty($_POST['email'])){
             $name = $_POST['name'];
-            $pass = md5($_POST['pass']);
+            $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
             $email    = $_POST['email'];
             $group_id = $_POST['group_id'];
 
