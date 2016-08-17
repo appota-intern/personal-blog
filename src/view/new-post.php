1b21a74937ae622dde818c7cf1e53d206e6106f2
thnt <coder53@gmail.com>
2016-08-17 17:02:46 +0700

    update new-post view

--- src/view/new-post.php
+++ src/view/new-post.php
@@ -1,7 +1,22 @@
 <?php $this->load('header', ['title' => $title]) ?>
+
+<div class="jumbotron jumbotron-new-post">
+    <h1 class="text-center">Write a new post</h1>
+</div>
 <div class="main-content">
     <div class="container">
-        <div id="summernote"></div>
+        <div class="row">
+            <div class="col-xs-12">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <input class="form-control input-md" type="text" placeholder="Title">
+                    </div>
+                    <div id="summernote"></div>
+                    <button class="btn btn-primary btn-large"><i class="fa fa-save"></i> Save</button>
+                    <button class="btn btn-success btn-large"><i class="fa fa-send"></i> Publish</button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
