5ca2e874a0037616607bd1493a509abbb365f5c7
trangnguyenit <trangnguyen95.hust@gmail.com>
2016-08-19 10:15:58 +0700

    post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -30,39 +30,31 @@ class PostController extends BaseController
 
     public function post(){
         session_start();
-        if(!empty($_POST['title']) && !empty($_POST['content'])){
+        
+        if(isset($_SESSION['loggedin'])){
+
+            if(!empty($_POST['title']) && !empty($_POST['content'])){
             $title   = $_POST['title'];
             $content = $_POST['content'];
 
             $user_id = $_SESSION['loggedin'];
-            // if(isset($_POST['status'])){
-            //     $status = 'published';
-            // }
-            // if(isset($_POST['save'])){
-            //     $status = 'saved';
-            // }
-
-            // if(isset($_POST['delete'])){
-            //     $status = 'delete';
-            // }
 
             $status = 'saved';
 
             // $post = new \Model\PostModel();
             $res = $this->postModel->addPost($user_id, $title, $content, $status);
 
-            // var_dump($res);
-
-            // if($res){
-            //     $this->redirect("/new-post");
-            // }
-
             $this->redirect("/new-post");
+            }
+            else{
+                $this->view->load('new-post', [
+                    'title' => 'Create new post'
+                ]);
+            }
         }
-        else{
-            $this->view->load('new-post', [
-                'title' => 'Create new post'
-            ]);
-        }
+       else{
+            echo "error";
+       }
+        
     }
 }
